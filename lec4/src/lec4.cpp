5a1cea8ff5d4b3eaa1cdc39cf6ab99fe3f01d80f
Jesse A. Tov <jesse.tov@gmail.com>
2016-01-15 22:47:01 -0600

    post-lecture 1/14

--- lec4/src/lec4.cpp
+++ lec4/src/lec4.cpp
@@ -1,10 +1,34 @@
 #include "eecs230.h"
 
+// Squares a number.
+int square(int x)
+{
+    return x * x;
+}
+
+// Computes the hypotenuse of a right triangle given the other
+// two sides.
+//
+// Examples:
+//
+//   hypotenuse(3, 4)  ==  5
+//   hypotenuse(3, -4) ==  5
+//   hypotenuse(12, 5) ==  13
+//
+double hypotenuse(double a, double b) {
+    return sqrt(square(a) + square(b));
+}
+
 int main() {
-    double a, b;
+    vector<double> fs;
+    double f;
 
-    cin >> a >> b;
-    if (!cin) simple_error("Could not read input.");
+    while (cin >> f) {
+        if (f == 0) break;
+        fs.push_back(f);
+    }
 
-    cout << sqrt(a * a + b * b) << '\n';
+    for (int i = 0; i < fs.size(); ++i)
+        cout << fs[i] << '\n';
+
 }
