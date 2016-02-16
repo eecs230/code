4d0da5ee087e77ea5677787622be3a7364760161
Jesse A. Tov <jesse.tov@gmail.com>
2016-02-16 14:04:01 -0600

    After class 2/16

--- lec11/src/Int_vector.cpp
+++ lec11/src/Int_vector.cpp
@@ -1,27 +1,87 @@
 #include <Int_vector.h>
+#include <algorithm>
+#include <iostream>
 
-constexpr size_t initial_capacity = 10;
+constexpr size_t default_initial_capacity = 8;
 
-Int_vector::Int_vector() : Int_vector(initial_capacity)
-{ }
-
-Int_vector::Int_vector(size_t capacity)
-        : capacity_{capacity}
+Int_vector::Int_vector(size_t initial_capacity)
+        : capacity_{initial_capacity}
         , size_{0}
-        , data_{new int[capacity]}
+        , data_{new int[initial_capacity]}
 { }
+
+Int_vector::Int_vector()
+        : Int_vector(default_initial_capacity)
+{ }
+
+int Int_vector::at(size_t n) const
+{
+    check_index_(n);
+    return *(data_ + n);
+}
+
+int& Int_vector::at(size_t n)
+{
+    check_index_(n);
+    return *(data_ + n);
+}
+
+void Int_vector::check_index_(size_t index) const
+{
+    if (index >= size_) throw range_error{};
+}
+
+int Int_vector::operator[](size_t n) const
+{
+    return *(data_ + n);
+}
+
+int& Int_vector::operator[](size_t n)
+{
+    return *(data_ + n);
+}
 
 size_t Int_vector::size() const
 {
     return size_;
 }
 
-int Int_vector::operator[](size_t index) const
+void Int_vector::push_back(int i)
 {
-    return data_[index];
+    ensure_capacity_(size_ + 1);
+    *(data_ + size_) = i;
+    ++size_;
 }
 
-int& Int_vector::operator[](size_t index)
+void Int_vector::ensure_capacity_(size_t req_capacity)
 {
-    return data_[index];
+    if (req_capacity <= capacity_) return;
+
+    size_t new_capacity = std::max(req_capacity, 2 * capacity_);
+    int* new_data       = new int[new_capacity];
+
+    for (int i = 0; i < size_; ++i) {
+        *(new_data + i) = *(data_ + i);
+    }
+
+    delete[] data_;
+
+    data_     = new_data;
+    capacity_ = new_capacity;
+}
+
+Int_vector::~Int_vector()
+{
+    std::cout << "goodbye\n";
+    delete[] data_;
+}
+
+Int_vector::Int_vector(const Int_vector& old)
+        : capacity_{old.capacity_}
+        , size_{old.size_}
+        , data_{new int[old.capacity_]}
+{
+    for (size_t i = 0; i < old.size(); ++i) {
+        *(data_ + i) = *(old.data_ + i);
+    }
 }
--- lec11/src/Int_vector.h
+++ lec11/src/Int_vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <stdexcept>
 
 class Int_vector
 {
@@ -10,10 +11,29 @@ class Int_vector
 
 public:
     Int_vector();
-    Int_vector(size_t capacity);
+    Int_vector(size_t initial_capacity);
+
+    Int_vector(const Int_vector&);
+
+    // Destructor runs when we're done with a vector
+    ~Int_vector();
 
     size_t size() const;
 
+    void push_back(int);
+
+    int at(size_t index) const;
+    int& at(size_t index);
+
     int operator[](size_t index) const;
     int& operator[](size_t index);
+
+private:
+    // Throw if index is out of bounds
+    void check_index_(size_t index) const;
+    // Make sure there's enough room for `size` elements
+    void ensure_capacity_(size_t req_capacity);
 };
+
+struct range_error : public std::exception
+{ };
