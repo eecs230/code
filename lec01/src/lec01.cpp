7340438b09af216c85d893cfe31812393373ab3c
Jesse A. Tov <jesse.tov@gmail.com>
2016-03-31 18:54:10 -0500

    after class

--- lec01/src/lec01.cpp
+++ lec01/src/lec01.cpp
@@ -1,5 +1,15 @@
-#include "eecs230.h"
+#include <eecs230.h>
 
-int main() {
-    cout << "Hello world!\n";
+int main()
+{
+    string username;
+
+    cout << "Please enter your name:\n> ";
+    cin >> username;
+
+    if (username == "Jesse") {
+        simple_error("No greetings for you.");
+    }
+
+    cout << "Hello, " << username << "\n";
 }
