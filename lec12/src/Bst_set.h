9ab6ea33441d99e53b5a9eaca593edf5cc67419f
Jesse A. Tov <jesse.tov@gmail.com>
2016-05-13 16:29:53 -0500

    Added remove.

---
+++ lec12/src/Bst_set.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Bst_set
+{
+public:
+    Bst_set() = default;
+    Bst_set(std::initializer_list<int>);
+
+    bool is_empty() const;
+    size_t size() const;
+
+    bool contains(int) const;
+    void insert(int);
+    void remove(int);
+
+    std::ostream& debug(std::ostream&);
+
+private:
+    struct Node;
+    using link_t = std::shared_ptr<Node>;
+
+    link_t root_ = nullptr;
+    size_t size_ = 0;
+};
