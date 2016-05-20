188db2fc7573bc92aacba80cd8432255d38e0734
Jesse A. Tov <jesse.tov@gmail.com>
2016-05-20 17:54:06 -0500

    Nicer demo.

--- lec14/test/circles.cpp
+++ lec14/test/circles.cpp
@@ -22,22 +22,6 @@ int main()
 {
     raster raster(width, height);
 
-//    auto c1s = circle({300, 225}, 200);
-//    auto c1  = gradient(c1s, color{1, 1, 0}, color{0, 1, 0, .2}, Circular{});
-//
-//    auto c2  = opacity(gradient(circle({500, 225}, 200),
-//                                color{.5, 0, 0}, color::black,
-//                                Circular{}, Sinusoidal{10, .25}),
-//                       0.5);
-//
-//    auto c3  = transform(opacity(gradient(circle({400, 381}, 200),
-//                                          color{0, 0, 1}, color::white,
-//                                          Vertical{}), 0.7),
-//                         affinity::rotation(10)(affinity::dilation(2, .25))
-//                                 .centered_on(400, 381));
-//
-//    auto drawing = overlay({ c3, c2, c1, background(color::black) });
-
     auto red     = fill(circle({100, 100}, 100), color{1, 0, 0});
     auto green   = fill(circle({300, 100}, 100), color{0, 1, 0});
     auto blue    = fill(circle({500, 100}, 100), color{0, 0, 1});
@@ -64,14 +48,14 @@ int main()
     auto row2    = overlay({cyan, magenta, yellow, black});
     auto row3    = overlay({c1, c2, c3, c4});
 
-    row2 = opacity(row2, 0.5);
-    row2 = transform(row2, affinity::rotation(.3).centered_on(400, 300));
+//    row2 = opacity(row2, 0.5);
+//    row2 = transform(row2, affinity::rotation(.3).centered_on(400, 300));
 
     auto drawing = overlay({row2, row1, row3,
                             background(color{.5, .5, .5})});
 
-    simple_render(*drawing, raster);
+    render(*drawing, raster);
 
     raster.write_bmp("circles.bmp");
-    system("open circles.bmp");
+    system("open circles.bmp"); // This works on OS X.
 }
