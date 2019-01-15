90f4a22310be0fa558b9d77a929e95a99fdc8aca
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-15 11:23:39 -0600

    Added to doc comments.

--- lec03/collisions.py
+++ lec03/collisions.py
@@ -9,13 +9,19 @@ from geometry import Circle, Position
 
 
 def input_position(prompt: str) -> Position:
-    """Reads a Position from the standard input."""
+    """Reads a Position from the standard input.
+
+    Expects two numbers (x, y) on one line separated by space.
+    """
     x, y = input(prompt).split()
     return Position(float(x), float(y))
 
 
 def input_circle() -> Circle:
-    """Reads a Circle from the standard input."""
+    """Reads a Circle from the standard input.
+
+    Reads the center position on one line, then the radius on the next.
+    """
     center = input_position("Enter the circle's center: ")
     radius = float(input("Enter the circle's radius: "))
     return Circle(center, radius)
