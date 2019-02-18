68fb619146c249c6f11b69b264273dd225a7317b
Jesse A. Tov <jesse.tov@gmail.com>
2019-02-18 14:38:03 -0600

    A few more comments.

--- lec07/village_problem.py
+++ lec07/village_problem.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+"""
+In a particular village (of n families), each family has a 50% chance of
+wanting children, and when a family has one or more children, they have
+a 75% chance of wanting another. Additionally, children are sorted into
+boy-children and girl-children, and according to the village's rules,
+no family may have more than one boy-child.
+
+We want to model the village (given n) to answer questions like:
+
+ - What is the average number of children per family?
+ - What is the average number of boy children per family?
+ - What is the average number of girl children per family?
+ - What percentage of families with children end with a girl child?
+ - ...?
+"""
+
 from enum import Enum, auto
 from typing import List, Iterator
 import random as r
@@ -8,14 +24,19 @@ from lib230 import record, Factory
 
 
 class Child(Enum):
+    """Models a child."""
     GIRL = 'G'
     BOY = 'B'
 
     @classmethod
     def random(cls) -> 'Child':
+        """Returns child with equal probability of each gender."""
         return r.choice([Child.GIRL, Child.BOY])
 
     def is_final(self) -> bool:
+        """Checks whether this child is final, in the sense that no
+        more children are allowed after.
+        """
         return self is Child.BOY
 
     def __str__(self) -> str:
@@ -24,6 +45,12 @@ class Child(Enum):
 
 @record(init=False)
 class Family:
+    """Models a family.
+
+     - _pfirst - probability of first child
+     - _pmore - probability of each additional child
+     - _children - list of children
+    """
     _pfirst:   float
     _pmore:    float
     _children: List[Child]
@@ -36,15 +63,19 @@ class Family:
             self._children.append(Child.random())
 
     def _pwants_child(self) -> float:
+        """Determines the probability that this family wants a child."""
         return self._pmore if self._children else self._pfirst
 
     def _more_allowed(self) -> bool:
+        """Determines whether this family is allowed an additional child."""
         return not (self._children and self._children[-1].is_final())
 
     def __len__(self) -> int:
+        """Returns the number of children in the family."""
         return len(self._children)
 
     def __iter__(self) -> Iterator[Child]:
+        """Allows iterating over the children of the family."""
         return self._children.__iter__()
 
     def __str__(self) -> str:
@@ -54,9 +85,17 @@ class Family:
 
 @record(init=False)
 class Village:
+    """Models a village.
+
+     - _families - the list of families
+    """
+
     _families: List[Family]
 
     def __init__(self, n: int = 100, pfirst: float = 0.5, pmore: float = 0.75) -> None:
+        """Constructs a new, random village of the given size and with the given
+        reproduction probabilities.
+        """
         self._families = [ Family(pfirst, pmore) for _ in range(n) ]
 
     def __len__(self) -> int:
