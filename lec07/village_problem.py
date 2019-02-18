f9b4064c94bc380c876b0fc42ce526340e878841
Jesse A. Tov <jesse.tov@gmail.com>
2019-02-18 14:39:39 -0600

    Redacted.

--- lec07/village_problem.py
+++ lec07/village_problem.py
@@ -23,89 +23,23 @@ import random as r
 from lib230 import record, Factory
 
 
-class Child(Enum):
-    """Models a child."""
-    GIRL = 'G'
-    BOY = 'B'
-
-    @classmethod
-    def random(cls) -> 'Child':
-        """Returns child with equal probability of each gender."""
-        return r.choice([Child.GIRL, Child.BOY])
-
-    def is_final(self) -> bool:
-        """Checks whether this child is final, in the sense that no
-        more children are allowed after.
-        """
-        return self is Child.BOY
-
-    def __str__(self) -> str:
-        return self.value
-
-
-@record(init=False)
-class Family:
-    """Models a family.
-
-     - _pfirst - probability of first child
-     - _pmore - probability of each additional child
-     - _children - list of children
-    """
-    _pfirst:   float
-    _pmore:    float
-    _children: List[Child]
-
-    def __init__(self, pfirst: float, pmore: float) -> None:
-        self._pfirst = pfirst
-        self._pmore = pmore
-        self._children = []
-        while self._more_allowed() and r.random() < self._pwants_child():
-            self._children.append(Child.random())
-
-    def _pwants_child(self) -> float:
-        """Determines the probability that this family wants a child."""
-        return self._pmore if self._children else self._pfirst
-
-    def _more_allowed(self) -> bool:
-        """Determines whether this family is allowed an additional child."""
-        return not (self._children and self._children[-1].is_final())
-
-    def __len__(self) -> int:
-        """Returns the number of children in the family."""
-        return len(self._children)
-
-    def __iter__(self) -> Iterator[Child]:
-        """Allows iterating over the children of the family."""
-        return self._children.__iter__()
-
-    def __str__(self) -> str:
-        return 'Family({})'.format(
-            ''.join(str(child) for child in self._children))
-
-
-@record(init=False)
 class Village:
-    """Models a village.
-
-     - _families - the list of families
-    """
-
-    _families: List[Family]
+    """Models a village."""
 
     def __init__(self, n: int = 100, pfirst: float = 0.5, pmore: float = 0.75) -> None:
         """Constructs a new, random village of the given size and with the given
         reproduction probabilities.
         """
-        self._families = [ Family(pfirst, pmore) for _ in range(n) ]
+        pass
 
     def __len__(self) -> int:
-        return len(self._families)
+        pass
 
     def __iter__(self) -> Iterator[Family]:
-        return self._families.__iter__()
+        pass
 
     def __str__(self) -> str:
-        return ''.join(str(family) + '\n' for family in self._families)
+        pass
 
 
 if __name__ == '__main__':
